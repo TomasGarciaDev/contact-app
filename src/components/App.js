3772ec8d45fb3ee176148f6dd695b2e7533aa91a
Tomas Garcia <tomasgarciadev@gmail.com>
2022-03-15 22:13:07 +0100

    Contact array created and renderContactList setted up

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,24 @@ import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Tomas",
+      email: "samotgt@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Elena",
+      email: "ele.ortizap@gmail.com",
+    },
+  ]
+
   return (
     <div className="ui container">
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts}/>
     </div>
   );
 }
