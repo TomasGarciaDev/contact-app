44a9bf74cf4a6e5c078e79e337ba98d2173a7e22
Tomas Garcia <tomasgarciadev@gmail.com>
2022-03-24 21:19:53 +0100

    Using state for adding contacts

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "Tomas",
-      email: "samotgt@gmail.com",
-    },
-    {
-      id: "2",
-      name: "Elena",
-      email: "ele.ortizap@gmail.com",
-    },
-  ]
-
+  const [contacts, setContacts] = useState([]);
   return (
     <div className="ui container">
       <Header />
